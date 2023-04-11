b3ca7c13ed2724f9361860cefdb4420fa731afab
Josh <JoshuaAGKelley@gmail.com>
2023-04-11 23:05:02 +0100

    Added Text Module and demo cascade

--- src/app.ts
+++ src/app.ts
@@ -7,34 +7,102 @@ app.get("/", (req: any, res: any) => {
 })
 
 class HTTPTrigger {
+    cascades!: Array<any>;
+
     constructor(method: string, path: string) {
         let context = this;
 
         if (String(method).toUpperCase() === "GET") {
             app.get(path, (req: any, res: any) => {
+                res.locals.variables = {};
                 context.trigger(req, res);
             })
         }
     }
 
     async trigger(req: any, res: any) {
-        let testModule = new HTTPResponse(res, "This is some awd");
-        testModule.execute();
+        for (let i = 0; i < this.cascades.length; i++) {
+            for (let needle = 0; needle < this.cascades[i].length; needle++) {
+                let module = this.cascades[i][needle];
+                let moduleType: string = module.moduleType;
+                let moduleClass = moduleDictionary[moduleType as keyof typeof moduleDictionary];
+                let currentModule = new moduleClass(module);
+
+                currentModule.execute({
+                    "cascade": this.cascades[i],
+                    "req": req,
+                    "res": res
+                });
+            }
+        }
     }
 }
 
-class HTTPResponse {
-    res: any;
-    data: string;
+interface CascadeComponent {
+    moduleType: string;
+    outputVariableName: string;
+    parameters: any;
+}
 
-    constructor(res: any, data: string) {
-        this.res = res;
-        this.data = data;
+class ModuleHTTPResponse {
+    inputVariable: any;
+
+    constructor(module: CascadeComponent) {
+        this.inputVariable = module.parameters.inputVariable;
     }
 
-    execute() {
-        this.res.send(this.data);
+    execute(props: any) {
+        let res = props.res;
+        let data;
+
+        if (this.inputVariable.type === "variable") {
+            data = res.locals.variables[this.inputVariable.value];
+        } else {
+            data = this.inputVariable.value;
+        }
+
+        res.send(data);
     }
 }
+
+class ModuleText {
+    value!: string;
+    outputVariableName!: string;
+
+    constructor(module: CascadeComponent) {
+        this.outputVariableName = module.outputVariableName;
+        this.value = module.parameters.value;
+    }
+
+    execute(props: any) {
+        let res = props.res;
+        res.locals.variables[this.outputVariableName] = this.value;
+    }
+}
+
+const demoCascade = [
+    {
+        moduleType: "ModuleText",
+        outputVariableName: "Text",
+        parameters: {
+            value: "Hello world!"
+        }
+    },
+    {
+        moduleType: "ModuleHTTPResponse",
+        parameters: {
+            inputVariable: {
+                type: "variable",
+                value: "Text"
+            }
+        }
+    }
+]
 
 let testTrigger = new HTTPTrigger("get", "/test");
+testTrigger.cascades = [demoCascade];
+
+const moduleDictionary = {
+    "ModuleText": ModuleText,
+    "ModuleHTTPResponse": ModuleHTTPResponse
+}
