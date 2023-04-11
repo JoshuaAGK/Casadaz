936bef328eb0176bce78880e110c713e5cd2c2c9
Josh <JoshuaAGKelley@gmail.com>
2023-04-12 00:10:01 +0100

    Removed hello world

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,6 @@ const express = require('express');
 const app = express();
 app.listen(3000);
 
-app.get("/", (req: any, res: any) => {
-    res.send("Hello world!");
-})
-
 const demoCascade = [
     {
         moduleType: "ModuleText",
