07fb0ce01bf17c73ca8f9777d11bb400f2a52977
Josh <JoshuaAGKelley@gmail.com>
2023-04-11 18:34:23 +0100

    Converted to TypeScript

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,22 @@ const express = require('express');
 const app = express();
 app.listen(3000);
 
-app.get("/", (req, res) => {
+app.get("/", (req: any, res: any) => {
     res.send("Hello world!");
 })
 
 class HTTPTrigger {
-    constructor(method, path) {
+    constructor(method: string, path: string) {
         let context = this;
 
         if (String(method).toUpperCase() === "GET") {
-            app.get(path, (req, res) => {
+            app.get(path, (req: any, res: any) => {
                 context.trigger(req, res);
             })
         }
     }
 
-    async trigger(req, res) {
+    async trigger(req: any, res: any) {
         res.send("This is some data.")
     }
 }
