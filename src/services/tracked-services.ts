726fdd5ee56742b167b15214f9d3e4470d3914bf
Josh <JoshuaAGKelley@gmail.com>
2023-04-14 01:10:02 +0100

    Set strict trigger & cascade types

--- src/services/tracked-services.ts
+++ src/services/tracked-services.ts
@@ -1,4 +1,9 @@
-let trackedTriggers: Array<any> = [];
-let trackedCascades: Array<any> = [];
+import Cascade from "../classes/cascade";
+import TriggerHTTP from "../classes/triggers/trigger-HTTP";
+
+type Triggers = TriggerHTTP; // Add more triggers here as created
+
+let trackedTriggers: Array<Triggers> = [];
+let trackedCascades: Array<Cascade> = [];
 
 export { trackedTriggers, trackedCascades };
