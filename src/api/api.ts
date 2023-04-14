05ec080d5a1e8a1dacab48ff2201841c07f46961
Josh <JoshuaAGKelley@gmail.com>
2023-04-14 01:29:49 +0100

    Add cascade method

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 const router: Router = express.Router();
 import client from "../config/mongodb";
 import triggerDictionary from '../services/dictionaries/trigger-dictionary';
+import moduleDictionary from '../services/dictionaries/module-dictionary';
 import { trackedCascades, trackedTriggers } from "../services/tracked-services";
 import Cascade from "../classes/cascade";
 
@@ -36,6 +37,38 @@ router.post("/addtrigger", async (req: any, res: any) => {
     triggerObject.id = insertResult.insertedId.toString();
     trackedTriggers.push(triggerObject);
     console.log(trackedTriggers);
+
+    res.send("ok");
+})
+
+router.post("/addcascade", async (req: any, res: any) => {
+    const uid = req.body.uid;
+    const teamName = req.body.team;
+    const cascade = req.body.cascade;
+
+    const teamsCollection = client.db("global").collection("teams");
+    const teams = await teamsCollection.find({teamName}).toArray();
+
+    if (teams.length === 0) {
+        res.send("No team found");
+        return;
+    }
+
+    if (!teams[0].members.includes(uid)) {
+        res.send("User is not a member of team");
+        return;
+    }
+    
+    const cascadesCollection = client.db(teamName).collection("cascades");
+    const insertResult = await cascadesCollection.insertOne(cascade);
+
+    let cascadeObject = new Cascade;
+    
+    cascadeObject.modules = cascade.modules;
+
+    cascadeObject.id = insertResult.insertedId.toString();
+    trackedCascades.push(cascadeObject);
+    console.log(trackedCascades);
 
     res.send("ok");
 })
